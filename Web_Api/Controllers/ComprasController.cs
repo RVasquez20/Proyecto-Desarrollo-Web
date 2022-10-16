ec668be12dfae121b7ab2942548739aba858b7b9
blitxs1226 <javier_minches@hotmail.com>
2022-10-15 19:31:30 -0600

    Tercera Parte

---
+++ Web_Api/Controllers/ComprasController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Api.Models;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+        private readonly ClinicaMedicaContext _context;
+
+        public ComprasController(ClinicaMedicaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Compras
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblCompra>>> GetTblCompras()
+        {
+            return Ok(await _context.TblCompras.ToListAsync());
+        }
+
+        // POST: api/Compras
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblCompra>> PostTblCompra(TblCompra tblCompra)
+        {
+            _context.TblCompras.Add(tblCompra);
+            await _context.SaveChangesAsync();
+
+            return Ok(tblCompra);
+        }
+
+        // DELETE: api/Compras/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblCompra(int id)
+        {
+            var tblCompra = await _context.TblCompras.FindAsync(id);
+            if (tblCompra == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblCompras.Remove(tblCompra);
+            await _context.SaveChangesAsync();
+
+            return Ok(tblCompra);
+        }
+
+        private bool TblCompraExists(int id)
+        {
+            return _context.TblCompras.Any(e => e.IdCompras == id);
+        }
+    }
+}
---
+++ Web_Api/Controllers/VentasDetalleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_Api.Models;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentasDetalleController : ControllerBase
+    {
+        private readonly ClinicaMedicaContext _context;
+
+        public VentasDetalleController(ClinicaMedicaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VentasDetalle
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblVentasDetalle>>> GetTblVentasDetalles()
+        {
+            return Ok(await _context.TblVentasDetalles.ToListAsync());
+        }
+
+        // POST: api/VentasDetalle
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblVentasDetalle>> PostTblVentasDetalle(TblVentasDetalle tblVentasDetalle)
+        {
+            _context.TblVentasDetalles.Add(tblVentasDetalle);
+            await _context.SaveChangesAsync();
+
+            return Ok(tblVentasDetalle);
+        }
+
+        private bool TblVentasDetalleExists(int id)
+        {
+            return _context.TblVentasDetalles.Any(e => e.IdVentasDetalle == id);
+        }
+    }
+}
